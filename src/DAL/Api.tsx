100b30e26b165442460c8c3fc2140e42c4f67dcf
Yossi <yossipoli@gmail.com>
2023-11-09 23:08:52 +0200

    add first api for getting data

---
+++ src/DAL/Api.tsx
@@ -0,0 +1,12 @@
+const link = 'https://rickandmortyapi.com/api/character'
+
+export const getCharacters = async (
+	page: number,
+	name: string,
+	status: string
+) => {
+	const characters = await fetch(`${link}`).then((response) => response.json())
+	return characters.results
+}
+
+//          ?page=1&name=rick&status=alive`
